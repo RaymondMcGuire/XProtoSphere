e88cdda4f3dcb3678283f973b93cb4dc6d2df518
raymondmg <wang.xu@ist.hokudai.ac.jp>
2026-03-06 10:55:06 +0900

    update

--- scripts/run_example.bat
+++ scripts/run_example.bat
@@ -2,14 +2,14 @@
 
 cd ..
 
-set input_model_name="bunny"
-set input_model_scale=0.6
-set distribution_type="A"
+set input_model_name="cube"
+set input_model_scale=0.3
+set distribution_type="C"
 set protosphere_max_iter=10
 set enable_dem_relax=1
 set overlap_ratio=0.6
 set dsr_max_iter=1000
-set enable_dist_constain=0
+set enable_dist_constain=1
 
 call .\build\bin\Release\xprotosphere.exe %input_model_name% %input_model_scale% %distribution_type% %protosphere_max_iter% %enable_dem_relax% %overlap_ratio% %dsr_max_iter% %enable_dist_constain%
 
